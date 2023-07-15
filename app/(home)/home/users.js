313b24882dcd77c7071bdb7d0524ed0d161075c9
Juliana Marie Agulto <57354191+xJMarie@users.noreply.github.com>
2023-07-15 22:20:41 +0800

    retrieve all users

--- app/(home)/home/users.js
+++ app/(home)/home/users.js
@@ -1,11 +1,49 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default function users() {
+
+export default function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await axios.get('/api/users');
+        if (response.status === 200) {
+          const data = response.data;
+          setUsers(data.users);
+        } else {
+          console.error('Error retrieving users:', response.status);
+        }
+      } catch (error) {
+        console.error('Error retrieving users:', error);
+      }
+    }
+
+    fetchUsers();
+  }, []);
   return (
-    <div className="RowList">
-      <div className="Row">
-        <p>This is the first user</p>
+    <div className='w-full'>
+      <h1>List of Users</h1>
+      <div className="flex items-center my-2 text-base ">
+        <table className="w-full max-w-lg shadow-md">
+          <thead>
+            <tr>
+              <th className="px-4 py-2 border-b">Name</th>
+              <th className="px-4 py-2 border-b">Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td className="px-4 py-2 border-b">{user.name}</td>
+                <td className="px-4 py-2 border-b">{user.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
-  )
+  );
 }
