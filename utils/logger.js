7a3d26df405040a241fa77c90e080f9fe6edda40
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2023-07-16 20:09:13 +0800

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,12 +1,5 @@
-import logLevelData from "./log_levels";
 import pino from "pino";
 import pretty from "pino-pretty";
-
-const logLevels = new Map(Object.entries(logLevelData));
-
-export const getLogLevel = (logger) => {
-    return logLevels.get(logger) || logLevels.get("*") || "info";
-};
 
 export const getLogger = () => {
     const logger = pino(
