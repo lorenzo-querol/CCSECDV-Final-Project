e26ef9670bca7f4e1a25e41081b4b7150f2b08c1
Juliana Agulto <juliana.marie39@gmail.com>
2023-07-21 14:20:39 +0800

    Add dopdown

--- app/(home)/home/page.js
+++ app/(home)/home/page.js
@@ -10,7 +10,8 @@ import pic from "@/posts/Zoom Background 2.png";   // temp
 import {
    BiImage,
    BiVideoPlus,
-   BiSearch
+   BiSearch,
+   BiDotsVerticalRounded
 } from "react-icons/bi"
 
 import { AiOutlineHeart, AiFillHeart, AiOutlineMessage, AiFillCloseCircle } from "react-icons/ai"
@@ -20,6 +21,8 @@ export default function Home() {
    const [imageFile, setImageFile] = useState(null);
    const [imagePreview, setImagePreview] = useState(null);
    const [isLiked, setIsLiked] = useState(false); // State to track heart fill
+   const [showDropdown, setShowDropdown] = useState(false); // Dropdown
+
 
    // Function to handle image selection
    const handleImageChange = (e) => {
@@ -44,6 +47,10 @@ export default function Home() {
       setIsLiked((prevIsLiked) => !prevIsLiked);
    };
 
+   const toggleDropdown = () => {
+      setShowDropdown(!showDropdown);
+   };
+
    // Function to handle form submission
    const handleSubmit = (e) => {
       e.preventDefault(); // Prevent default form submission behavior
@@ -57,7 +64,7 @@ export default function Home() {
       <>
          <div className='flex flex-row h-full overflow-y-auto'>
             {/* Middle */}
-            <div className="flex-grow w-4/6 h-auto border border-t-0 border-gray-600">
+            <div className="flex-grow w-4/6 h-full border-t-0 border-gray-600 border-x-2">
                <div className="flex">
                   <div className="flex-1 m-2">
                      <h2 className="px-4 py-2 text-2xl font-bold text-white">Home</h2>
@@ -130,34 +137,58 @@ export default function Home() {
                <hr className="border-4 border-indigo-800" />
                <div></div>
 
-               {/* List of postss */}
-               <div className="">
-                  {/* posts */}
-                  <div>
-                     <div className="flex flex-shrink-0 p-4 pb-0 b">
-                        <a href="#" className="flex-shrink-0 block group">
-                           <div className="flex items-center">
-                              <div>
-                                 <Image
-                                    className="inline-block w-10 h-10 rounded-full"
-                                    src={control}
-                                    alt=""
-                                 />
-                              </div>
-                              <div className="ml-3">
-                                 <p className="text-base font-medium leading-6 text-white">
-                                    FirstName LastName
-                                    <span
-                                       className="text-sm font-medium leading-5 text-gray-400 transition duration-150 ease-in-out group-hover:text-gray-300"
-                                    >
-                                       . 16 April
-                                    </span>
-                                 </p>
+               {/* List of posts */}
+               <div className="flex-col ">
+                  {/* Post */}
+                  <div className="border-b-2 border-gray-600 ">
+                     <div className="flex flex-shrink-0 p-4 ">
+                        <div className="flex-grow">
+                           {/* Post: header */}
+                           <div className="flex items-center justify-between ">
+                              <div className="flex items-center">
+                                 {/* Image profile */}
+                                 <div>
+                                    <Image
+                                       className="inline-block w-10 h-10 rounded-full"
+                                       src={control}
+                                       alt=""
+                                    />
+                                 </div>
+                                 {/* Details */}
+                                 <div className="ml-3">
+                                    <p className="text-base font-medium leading-6 text-white">
+                                       FirstName LastName
+                                       <span className="text-sm font-medium leading-5 text-gray-400 transition duration-150 ease-in-out group-hover:text-gray-300">
+                                          . 16 April
+                                       </span>
+                                    </p>
+                                 </div>
                               </div>
                            </div>
-                        </a>
+                        </div>
+                        {/* Only show if its the user's post */}
+                        {/* Dropdown icon */}
+                        <div className="relative">
+                           <div className="flex items-center flex-shrink-0 ml-auto">
+                              <BiDotsVerticalRounded size={25} onClick={toggleDropdown} />
+                           </div>
+                           {showDropdown && (
+                              <div className="absolute right-0 mt-2 bg-white rounded shadow-md">
+                                 <button
+                                    onClick={() => {
+                                       console.log('Delete clicked!');
+                                    }}
+                                    className="block w-full px-4 py-2 text-left text-gray-800 rounded hover:bg-red-500 hover:text-white"
+                                 >
+                                    Delete
+                                 </button>
+                              </div>
+                           )}
+                        </div>
+
                      </div>
-                     <div className="pl-16 ">
+                     {/* Post: content */}
+                     <div className="pl-16">
                         <p className="flex-shrink w-auto text-base font-medium text-white">
                            Day 07 of the challenge
                            <span className="text-indigo-400">#100DaysOfCode</span> I was wondering
@@ -169,15 +200,16 @@ export default function Home() {
                         </p>
 
                         {/* Check if there's an image otherwise, show nothing. */}
-                        <div className="mt-2 bg-blue-500">
-                           <Image src={pic} alt="Image Preview" className="max-w-80 max-h-64" />
+                        <div className="relative mt-2">
+                           <Image src={pic} alt="Image Preview" className="w-full max-w-80 max-h-64" />
                         </div>
 
+                        {/* Post: Footer */}
                         {/* Icons */}
                         <div className="flex">
                            <div className="w-full">
                               <div className="flex items-center">
-                                 <div className="flex-1 ">
+                                 {/* <div className="flex-1 ">
                                     <div className="flex items-center justify-center flex-1 py-2 m-2 text-center">
                                        <a
                                           href="#"
@@ -186,24 +218,132 @@ export default function Home() {
                                           <AiOutlineMessage size={25} />
                                        </a>
                                     </div>
-                                 </div>
-                                 <div className="flex items-center justify-center flex-1 py-2 m-2 text-center">
+                                 </div> */}
+                                 <div className="flex flex-col items-center justify-center flex-1 py-2 m-2 space-x-2 text-center">
                                     <button
                                        onClick={handleLike}
-                                       className="flex items-center w-12 px-3 py-2 mt-1 text-base font-medium leading-6 text-gray-500 rounded-full group hover:bg-indigo-800 hover:text-indigo-300"
+                                       className="flex items-center w-12 px-3 py-1 mt-1 text-base font-medium leading-6 text-gray-500 rounded-full group hover:bg-indigo-800 hover:text-indigo-300"
                                     >
                                        {isLiked ? (
-                                          <AiOutlineHeart size={25} />
-                                       ) : (
                                           <AiFillHeart size={25} className="text-red-500" />
+                                       ) : (
+
+                                          <AiOutlineHeart size={25} />
                                        )}
                                     </button>
+                                    {/* Only show if there's at least 2 likes */}
+                                    <span className="text-sm text-gray-200">
+                                       Number of likes
+                                    </span>
                                  </div>
                               </div>
                            </div>
                         </div>
                      </div>
-                     <hr className="border-gray-600" />
+                     {/* <hr className="border-gray-600" /> */}
+                  </div>
+                  {/* Post */}
+                  <div>
+                     <div className="flex flex-shrink-0 p-4 ">
+                        <div className="flex-grow">
+                           {/* Post: header */}
+                           <div className="flex items-center justify-between ">
+                              <div className="flex items-center">
+                                 {/* Image profile */}
+                                 <div>
+                                    <Image
+                                       className="inline-block w-10 h-10 rounded-full"
+                                       src={control}
+                                       alt=""
+                                    />
+                                 </div>
+                                 {/* Details */}
+                                 <div className="ml-3">
+                                    <p className="text-base font-medium leading-6 text-white">
+                                       FirstName LastName
+                                       <span className="text-sm font-medium leading-5 text-gray-400 transition duration-150 ease-in-out group-hover:text-gray-300">
+                                          . 16 April
+                                       </span>
+                                    </p>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                        {/* Only show if its the user's post */}
+                        {/* Dropdown icon */}
+                        <div className="relative">
+                           <div className="flex items-center flex-shrink-0 ml-auto">
+                              <BiDotsVerticalRounded size={25} onClick={toggleDropdown} />
+                           </div>
+                           {showDropdown && (
+                              <div className="absolute right-0 mt-2 bg-white border rounded shadow-md">
+                                 <button
+                                    onClick={() => {
+                                       console.log('Delete clicked!');
+                                    }}
+                                    className="block w-full px-4 py-2 text-left text-gray-800 border-none hover:bg-red-500 hover:text-white"
+                                 >
+                                    Delete
+                                 </button>
+                              </div>
+                           )}
+                        </div>
+
+                     </div>
+                     {/* Post: content */}
+                     <div className="pl-16">
+                        <p className="flex-shrink w-auto text-base font-medium text-white">
+                           Day 07 of the challenge
+                           <span className="text-indigo-400">#100DaysOfCode</span> I was wondering
+                           what I can do with
+                           <span className="text-indigo-400">#tailwindcss</span>, so just started
+                           building Twitter UI using Tailwind and so far it looks so
+                           promising. I will post my code after completion. [07/100]
+                           <span className="text-indigo-400"> #WomenWhoCode #CodeNewbie</span>
+                        </p>
+
+                        {/* Check if there's an image otherwise, show nothing. */}
+                        <div className="relative mt-2">
+                           <Image src={pic} alt="Image Preview" className="w-full max-w-80 max-h-64" />
+                        </div>
+
+                        {/* Post: Footer */}
+                        {/* Icons */}
+                        <div className="flex">
+                           <div className="w-full">
+                              <div className="flex items-center">
+                                 {/* <div className="flex-1 ">
+                                    <div className="flex items-center justify-center flex-1 py-2 m-2 text-center">
+                                       <a
+                                          href="#"
+                                          className="flex items-center w-12 px-3 py-2 mt-1 text-base font-medium leading-6 text-gray-500 rounded-full group hover:bg-indigo-800 hover:text-indigo-300"
+                                       >
+                                          <AiOutlineMessage size={25} />
+                                       </a>
+                                    </div>
+                                 </div> */}
+                                 <div className="flex flex-col items-center justify-center flex-1 py-2 m-2 space-x-2 text-center">
+                                    <button
+                                       onClick={handleLike}
+                                       className="flex items-center w-12 px-3 py-2 mt-1 text-base font-medium leading-6 text-gray-500 rounded-full group hover:bg-indigo-800 hover:text-indigo-300"
+                                    >
+                                       {isLiked ? (
+                                          <AiFillHeart size={25} className="text-red-500" />
+                                       ) : (
+
+                                          <AiOutlineHeart size={25} />
+                                       )}
+                                    </button>
+                                    {/* Only show if there's at least 2 likes */}
+                                    <span className="text-sm text-gray-200">
+                                       Number of likes
+                                    </span>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     {/* <hr className="border-gray-600" /> */}
                   </div>
                </div>
 
@@ -219,7 +359,7 @@ export default function Home() {
                      type="search"
                      name="search"
                      placeholder="Search Twitter"
-                     className="w-full h-10 px-10 pr-5 text-sm bg-indigo-800 border-0 rounded-full shadow focus:outline-none bg-purple-white"
+                     className="w-full h-10 px-10 pr-5 text-sm text-gray-200 bg-indigo-800 border-0 rounded-full shadow focus:outline-none"
                   />
                </div>
             </div>
