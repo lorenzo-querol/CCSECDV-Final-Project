beaad75faa1c24d40072148a535354aade042c9b
Lorenzo Querol <lorenzoquerol@gmail.com>
2023-07-22 23:25:23 +0800

    Update layout.js

--- app/(admin)/layout.js
+++ app/(admin)/layout.js
@@ -1,17 +1,11 @@
 "use client";
 
 import { AiFillHome, AiFillMessage, AiOutlineLogout } from "react-icons/ai";
-import {
-    BsFillGearFill
-} from "react-icons/bs";
 import React, { useState } from "react";
 import { signOut, useSession } from "next-auth/react";
 
-import {
-    FaListAlt
-} from "react-icons/fa";
-
-
+import { BsFillGearFill } from "react-icons/bs";
+import { FaListAlt } from "react-icons/fa";
 import { FaUsers } from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
@@ -31,10 +25,10 @@ export default function Sidebar({ children }) {
 	const [close, setClose] = useState(false);
 	const [activeMenuItem, setActiveMenuItem] = useState("Home"); // Set initial active menu item by title
 
-    const Menus = [
-        { title: "List of Users", icon: <FaListAlt />, path: "/list-of-users" },
-        { title: "Settings", icon: <BsFillGearFill />, path: "/settings" },
-    ];
+	const Menus = [
+		{ title: "List of Users", icon: <FaListAlt />, path: "/list-of-users" },
+		{ title: "Settings", icon: <BsFillGearFill />, path: "/settings" },
+	];
 
 	const handleMenuItemClick = (title, path) => {
 		setActiveMenuItem(title);
@@ -44,71 +38,70 @@ export default function Sidebar({ children }) {
 		return <div>Loading...</div>;
 	}
 
-    return (
-        <div className="fixed left-0 flex w-full h-screen">
-            <div
-                className={`${close ? "w-20" : "w-72"
-                    } relative h-screen bg-indigo-900 p-5 pt-8 duration-300 flex flex-col justify-start`}
-            >
-                <Image
-                    src={control}
-                    alt="Control Icon"
-                    className={`absolute -right-3 top-9 w-7 cursor-pointer rounded-full border-2 border-indigo-900  ${!close && "rotate-180"
-                        }`}
-                    onClick={() => setClose(!close)}
-                    width={24}
-                    height={24}
-                />
-                <div className="flex items-center gap-x-4">
-                    <h1
-                        className={`origin-left text-3xl font-medium text-white duration-200 ${close && "scale-0"
-                            }`}
-                    >
-                        THOUGHTS.
-                    </h1>
-                </div>
-                <ul className="flex-grow pt-6">
-                    {Menus.map((Menu, index) => (
-                        <li
-                            key={index}
-                            className={`flex cursor-pointer items-center gap-x-4 rounded-md p-2 text-xl text-white hover:bg-indigo-700 ${Menu.gap ? "mt-9" : "mt-2"
-                                } ${Menu.title === activeMenuItem && "bg-indigo-700"
-                                }`} // Add condition to apply active class
-                            onClick={() => handleMenuItemClick(Menu.title)} // Pass the title to the click event handler
-                        >
-                            <Link
-                                href={Menu.path}
-                                className="flex items-center space-x-2"
-                            >
-                                {Menu.icon}
-                                <span
-                                    className={`${close && "hidden"
-                                        } origin-left duration-200`}
-                                >
-                                    {Menu.title}
-                                </span>
-                            </Link>
-                        </li>
-                    ))}
-                    <li
-                        className={`flex cursor-pointer items-center gap-x-4 rounded-md p-2 text-xl text-white hover:bg-indigo-700 mt-2`}
-                        onClick={() =>
-                            signOut({
-                                callbackUrl: "http://localhost:3000/login",
-                            })
-                        }
-                    >
-                        <div className="flex items-center space-x-2">
-                            <AiOutlineLogout />
-                            <span
-                                className={`${close && "hidden"
-                                    } origin-left duration-200`}
-                            >
-                                Log Out
-                            </span>
-                        </div>
-                    </li>
-                </ul>
+	return (
+		<div className="fixed left-0 flex w-full h-screen">
+			<div
+				className={`${
+					close ? "w-20" : "w-72"
+				} relative h-screen bg-indigo-900 p-5 pt-8 duration-300 flex flex-col justify-start`}
+			>
+				<Image
+					src={control}
+					alt="Control Icon"
+					className={`absolute -right-3 top-9 w-7 cursor-pointer rounded-full border-2 border-indigo-900  ${
+						!close && "rotate-180"
+					}`}
+					onClick={() => setClose(!close)}
+					width={24}
+					height={24}
+				/>
+				<div className="flex items-center gap-x-4">
+					<h1
+						className={`origin-left text-3xl font-medium text-white duration-200 ${
+							close && "scale-0"
+						}`}
+					>
+						THOUGHTS.
+					</h1>
+				</div>
+				<ul className="flex-grow pt-6">
+					{Menus.map((Menu, index) => (
+						<li
+							key={index}
+							className={`flex cursor-pointer items-center gap-x-4 rounded-md p-2 text-xl text-white hover:bg-indigo-700 ${
+								Menu.gap ? "mt-9" : "mt-2"
+							} ${Menu.title === activeMenuItem && "bg-indigo-700"}`} // Add condition to apply active class
+							onClick={() => handleMenuItemClick(Menu.title)} // Pass the title to the click event handler
+						>
+							<Link
+								href={Menu.path}
+								className="flex items-center space-x-2"
+							>
+								{Menu.icon}
+								<span
+									className={`${close && "hidden"} origin-left duration-200`}
+								>
+									{Menu.title}
+								</span>
+							</Link>
+						</li>
+					))}
+					<li
+						className={`flex cursor-pointer items-center gap-x-4 rounded-md p-2 text-xl text-white hover:bg-indigo-700 mt-2`}
+						onClick={() =>
+							signOut({
+								callbackUrl: "/login",
+							})
+						}
+					>
+						<div className="flex items-center space-x-2">
+							<AiOutlineLogout />
+							<span className={`${close && "hidden"} origin-left duration-200`}>
+								Log Out
+							</span>
+						</div>
+					</li>
+				</ul>
 
 				{/* Account */}
 				<div className="flex flex-shrink-0 mt-auto rounded-full hover:bg-blue-00">
