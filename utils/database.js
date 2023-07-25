6b4d850f58924a9b7105fdc1e6f976ff440ae6df
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2023-07-25 21:39:16 +0800

    update: migrate aws v2 to v3

--- utils/database.js
+++ utils/database.js
@@ -1,20 +1,20 @@
-import AWS from "aws-sdk";
+import { S3 } from "@aws-sdk/client-s3";
 import mysql from "serverless-mysql";
 
 export const database = mysql({
-	config: {
-		host: process.env.PLANETSCALE_HOST,
-		user: process.env.PLANETSCALE_USER,
-		password: process.env.PLANETSCALE_PASSWORD,
-		database: process.env.PLANETSCALE_NAME,
-		ssl: {
-			rejectUnauthorized: true,
-		},
-	},
+    config: {
+        host: process.env.PLANETSCALE_HOST,
+        user: process.env.PLANETSCALE_USER,
+        password: process.env.PLANETSCALE_PASSWORD,
+        database: process.env.PLANETSCALE_NAME,
+        ssl: {
+            rejectUnauthorized: true,
+        },
+    },
 });
 
-export const s3 = new AWS.S3({
-	accessKeyId: process.env.S3_ACCESS_KEY,
-	secretAccessKey: process.env.S3_SECRET_KEY,
-	region: process.env.S3_REGION,
+export const s3 = new S3({
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SECRET_KEY,
+    region: process.env.S3_REGION,
 });
