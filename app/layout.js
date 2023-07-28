235325f2d135b7e3afe27e08abcc32fae7135d92
Lorenzo Querol <lorenzoquerol@gmail.com>
2023-07-29 05:38:45 +0800

    remove metadata

--- app/layout.js
+++ app/layout.js
@@ -4,18 +4,12 @@ import "./globals.css";
 
 import { SessionProvider } from "next-auth/react";
 
-export const metadata = {
-    title: "Sling Academy",
-    description:
-        "This is a meta description. Welcome to slingacademy.com. Happy coding and have a nice day",
-};
-
 export default function RootLayout({ children }) {
-    return (
-        <html className="relative w-full h-screen">
-            <body className="flex items-center justify-center h-full">
-                <SessionProvider>{children}</SessionProvider>
-            </body>
-        </html>
-    );
+	return (
+		<html className="relative w-full h-screen">
+			<body className="flex items-center justify-center h-full">
+				<SessionProvider>{children}</SessionProvider>
+			</body>
+		</html>
+	);
 }
