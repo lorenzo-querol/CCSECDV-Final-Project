11ac8ad42232509e568c905a1b81547bdf2b2aa6
Lorenzo Querol <lorenzoquerol@gmail.com>
2023-08-01 14:39:12 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: "/",
-                destination: "/login",
-                permanent: true,
-            },
-        ];
-    },
-    images: {
-        domains: ["cssecdv-final-project.s3-ap-southeast-1.amazonaws.com"],
-    },
-    webpack: (config, { isServer, nextRuntime }) => {
-        if (isServer && nextRuntime === "nodejs")
-            config.plugins.push(
-                new webpack.IgnorePlugin({ resourceRegExp: /^aws-crt$/ })
-            );
-        return config;
-    },
+	async redirects() {
+		return [
+			{
+				source: "/",
+				destination: "/login",
+				permanent: true,
+			},
+		];
+	},
+	images: {
+		domains: ["cssecdv-final-project.s3-ap-southeast-1.amazonaws.com"],
+	},
 };
 
 module.exports = nextConfig;
