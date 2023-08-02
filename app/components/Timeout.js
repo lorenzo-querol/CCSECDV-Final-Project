8d59e1d2826da10855aa922654f424ca07c9889e
Juliana Marie Agulto <juliana.marie39@gmail.com>
2023-08-02 14:30:01 +0800

    Add timeout page

---
+++ app/components/Timeout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Timeout() {
+   return (
+      <div className="flex items-center justify-center h-full">
+         <span className='text-2xl font-bold'>Your account has been reported.</span>
+         <p>Please wait a few moments then try again.</p>
+      </div>
+   )
+}
