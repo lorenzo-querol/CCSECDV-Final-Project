1241db5fbb79f844cd50666939f56b64eba35708
Juliana Marie Agulto <juliana.marie39@gmail.com>
2023-08-02 15:44:47 +0800

    Fix: timeout UI

--- app/components/Timeout.js
+++ app/components/Timeout.js
@@ -3,8 +3,10 @@ import React from 'react'
 export default function Timeout() {
    return (
       <div className="flex items-center justify-center h-full">
-         <span className='text-2xl font-bold'>Your account has been reported.</span>
-         <p>Please wait a few moments then try again.</p>
+         <div className='flex-col'>
+            <span className='text-2xl font-bold'>Your account has been reported.</span>
+            <p className='text-center'>Please wait a few moments then try again.</p>
+         </div>
       </div>
-   )
+   );
 }
